5d193999fe54790ef58d5e2f15f58337f17e295e
ohad1992 <50565483+ohad1992@users.noreply.github.com>
2020-08-09 16:08:32 +0300

    Create Tip.js

---
+++ Tip.js
@@ -0,0 +1,33 @@
+function  TipFunction() {
+
+    var Bill = document.getElementById("Bill").value;
+    var Service = document.getElementById("Service").value;
+    var Sharing = document.getElementById("Sharing").value;
+
+    if(Bill===''|| Service==0){ // check if bill or service =0
+        alert("please enter values");
+        return;
+    }
+
+    if(Sharing<1|| Sharing===''){ // check if sharing <1
+         Sharing=1;
+    }
+
+    var total= Bill * Service/ Sharing ;
+  var total=Math.ceil(total);
+
+    document.getElementById("tip").innerHTML = total;
+    addEventListener("keyup", function(event) { // let enter key run the func
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.keyCode === 13) {
+            // Cancel the default action, if needed
+            event.preventDefault();
+            // Trigger the button element with a click
+            document.getElementById("calculate").click();
+        }
+    });
+}
+
+
+
+
